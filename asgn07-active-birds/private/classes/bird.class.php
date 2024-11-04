6d9ecc92de74e8d63bea34ce159ce778447f79b8
Sydney Penix <sydneyrpenix@students.abtech.edu>
2024-11-03 21:58:17 -0500

    Add functions for finding by sql, find_all(); ;

--- asgn07-active-birds/private/classes/bird.class.php
+++ asgn07-active-birds/private/classes/bird.class.php
@@ -6,9 +6,25 @@ class Bird
 
   static protected $database;
 
-  static public function set_database($database) {
-    self::$database = $database;
+  static public function find_by_sql($sql) {
+    $result = self::$database->query($sql);
+    if(!$result) {
+      exit("Database query failed.");
+    }
+
+    $object_array = [];
+    while($record = $result->fetch_assoc()) {
+      $object_array[] = self::instantiate($record);
+    }
+    $result = free();
+    return $object_array;
   }
+
+  static public function find_all() {
+    $sql = "SELECT * FROM sabirds";
+    return self::find_by_sql($sql);
+  }
+
 // Active Record Code End 
 
   public $common_name;
