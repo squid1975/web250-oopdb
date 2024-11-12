27ad902dd1ff914f5a6f8cfd539e6f6ca1715e3d
Sydney Penix <sydneyrpenix@students.abtech.edu>
2024-11-12 17:10:03 -0500

    create form_fields.php, changed protected const to public

--- asgn08-bird-create-update/public/active-record/form_fields.php
+++ asgn08-bird-create-update/public/active-record/form_fields.php
@@ -1,8 +1,40 @@
 <?php
 
-/* 
-  Use the bicycles/staff/form_fields.php file as a guide 
-  so your file mimics the same functionality.
- 
-*/
+// prevents this code from being loaded directly in the browser
+// or without first setting the necessary object
+if(!isset($bird)) {
+  redirect_to(url_for('/index.php'));
+}
+?>
 
+<dl>
+  <dt>Common Name</dt>
+  <dd><input type="text" name="common_name" value="<?php echo h($bird->common_name); ?>" ></dd>
+</dl>
+
+<dl>
+  <dt>Habitat</dt>
+  <dd><input type="text" name="habitat" value="<?php echo h($bird->habitat);?>" ></dd>
+</dl>
+
+<dl>
+  <dt>Food</dt>
+  <dd><input type="text" name="food" value="<?php echo h($bird->food);?>" ></dd>
+</dl>
+
+<dl>
+  <dt>Conservation Level</dt>
+  <dd>
+    <select name="conservation_id">
+      <option value=""></option>
+    <?php foreach(Bird::CONSERVATION_OPTIONS as $conservation_id=> $conservation_value) { ?>
+      <option value="<?php echo $conservation_id; ?>"><?php echo $conservation_value; ?></option>
+    <?php } ?>
+    </select>
+  </dd>
+</dl>
+
+<dl>
+  <dt>Backyard Tips</dt>
+  <dd><textarea name="backyard_tips" rows="5" cols="50"><?php echo h($bird->backyard_tips);?></textarea></dd>
+</dl>
