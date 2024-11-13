c9377a5b75148b2cac6da8762161686e6209be42
Sydney Penix <sydneyrpenix@students.abtech.edu>
2024-11-12 19:45:05 -0500

    Create delete.php

--- asgn08-bird-create-update/public/active-record/delete.php
+++ asgn08-bird-create-update/public/active-record/delete.php
@@ -2,10 +2,48 @@
 
 require_once('../private/initialize.php');
 
-/* 
-  Use the bicycles/staff/delete.php file as a guide 
-  so your file mimics the same functionality.
-*/
+if(!isset($_GET['id'])) {
+  redirect_to(url_for('/public/birds.php'));
+}
+$id = $_GET['id'];
+$bird = Bird::find_by_id($id);
+  if($bird === false){
+    redirect_to(url_for('/public/birds.php'));
+  }
+
+if(is_post_request()) {
+
+  // Delete bird
+  $result = $bird->delete();
+  $_SESSION['message'] = 'The bird was deleted successfully.';
+  redirect_to(url_for('/public/birds.php'));
+
+} else {
+  // Display form
+}
+
+?>
+
+<?php $page_title = 'Delete Bird'; ?>
+<?php include(SHARED_PATH . '/public_header.php'); ?>
+
+<div id="content">
+
+  <a href="<?php echo url_for('/public/birds.php'); ?>">&laquo; Back to List</a>
+
+  <div class="bird delete">
+    <h1>Delete Bird</h1>
+    <p>Are you sure you want to delete this bird?</p>
+    <p class="item"><?php echo h($bird->common_name); ?></p>
+
+    <form action="<?php echo url_for('/public/active-record/delete.php?id=' . h(u($id))); ?>" method="post">
+      <div id="operations">
+        <input type="submit" name="commit" value="Delete Bird" />
+      </div>
+    </form>
+  </div>
+
+</div>
 
 
 
