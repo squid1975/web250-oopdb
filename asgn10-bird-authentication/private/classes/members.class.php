0ce61eb5f06f6e7ba8dddbc0d4d2e1ced95f227f
Sydney Penix <sydneyrpenix@students.abtech.edu>
2024-11-25 13:28:14 -0500

    Create members class

---
+++ asgn10-bird-authentication/private/classes/members.class.php
@@ -0,0 +1,31 @@
+<?php 
+
+class Members extends DatabaseObject {
+
+  static protected $table_name = "users";
+  static protected $db_columns = ['id', 'first_name', 'last_name', 'email', 'username', 'user_level', 'hashed_password'];
+
+  public $id;
+  public $first_name;
+  public $last_name;
+  public $email;
+  public $username;
+  protected $hashed_password;
+  public $password;
+  public $confirm_password;
+
+  public function __construct($args=[]) {
+    $this->first_name = $args['first_name'] ?? '';
+    $this->last_name = $args['last_name'] ?? '';
+    $this->email = $args['email'] ?? '';
+    $this->username = $args['username'] ?? '';
+    $this->password = $args['password'] ?? '';
+    $this->confirm_password = $args['confirm_password'] ?? '';
+  }
+
+  public function full_name() {
+    return $this->first_name . " " . $this->last_name;
+  }
+
+  
+}
