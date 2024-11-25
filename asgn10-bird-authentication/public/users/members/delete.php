a9b53c8820aa612c7978018cb295e2a955d5b9a8
Sydney Penix <sydneyrpenix@students.abtech.edu>
2024-11-25 14:07:30 -0500

    Modify delete.php to reflect members

--- asgn10-bird-authentication/public/users/members/delete.php
+++ asgn10-bird-authentication/public/users/members/delete.php
@@ -3,20 +3,20 @@
 require_once('../../../private/initialize.php');
 
 if(!isset($_GET['id'])) {
-  redirect_to(url_for('/staff/admins/index.php'));
+  redirect_to(url_for('/users/members/index.php'));
 }
 $id = $_GET['id'];
-$admin = Admin::find_by_id($id);
-if($admin == false) {
-  redirect_to(url_for('/staff/admins/index.php'));
+$member = Members::find_by_id($id);
+if($member == false) {
+  redirect_to(url_for('/users/members/index.php'));
 }
 
 if(is_post_request()) {
 
   // Delete admin
-  $result = $admin->delete();
-  $_SESSION['message'] = 'The admin was deleted successfully.';
-  redirect_to(url_for('/staff/admins/index.php'));
+  $result = $member->delete();
+  $_SESSION['message'] = 'The member was deleted successfully.';
+  redirect_to(url_for('/users/members/index.php'));
 
 } else {
   // Display form
@@ -24,25 +24,25 @@ if(is_post_request()) {
 
 ?>
 
-<?php $page_title = 'Delete Admin'; ?>
-<?php include(SHARED_PATH . '/staff_header.php'); ?>
+<?php $page_title = 'Delete Member'; ?>
+<?php include(SHARED_PATH . '/public_header.php'); ?>
 
 <div id="content">
 
-  <a class="back-link" href="<?php echo url_for('/staff/admins/index.php'); ?>">&laquo; Back to List</a>
+  <a class="back-link" href="<?php echo url_for('/users/members/index.php'); ?>">&laquo; Back to List</a>
 
-  <div class="admin delete">
-    <h1>Delete Admin</h1>
-    <p>Are you sure you want to delete this admin?</p>
-    <p class="item"><?php echo h($admin->full_name()); ?></p>
+  <div class="member delete">
+    <h1>Delete Member</h1>
+    <p>Are you sure you want to delete this member?</p>
+    <p class="item"><?php echo h($member->full_name()); ?></p>
 
-    <form action="<?php echo url_for('/staff/admins/delete.php?id=' . h(u($id))); ?>" method="post">
+    <form action="<?php echo url_for('/users/members/delete.php?id=' . h(u($id))); ?>" method="post">
       <div id="operations">
-        <input type="submit" name="commit" value="Delete Admin" />
+        <input type="submit" name="commit" value="Delete Member" >
       </div>
     </form>
   </div>
 
 </div>
 
-<?php include(SHARED_PATH . '/staff_footer.php'); ?>
+<?php include(SHARED_PATH . '/public_footer.php'); ?>
