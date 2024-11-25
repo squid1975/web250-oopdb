02a8a39746b8d34fde95b3387bd6c43c4b3bae0d
Sydney Penix <sydneyrpenix@students.abtech.edu>
2024-11-25 14:14:58 -0500

    Modify edit and form_fields to member

--- asgn10-bird-authentication/public/users/members/edit.php
+++ asgn10-bird-authentication/public/users/members/edit.php
@@ -3,24 +3,24 @@
 require_once('../../../private/initialize.php');
 
 if(!isset($_GET['id'])) {
-  redirect_to(url_for('/staff/admins/index.php'));
+  redirect_to(url_for('/users/members/index.php'));
 }
 $id = $_GET['id'];
-$admin = Admin::find_by_id($id);
-if($admin == false) {
-  redirect_to(url_for('/staff/admins/index.php'));
+$member = Members::find_by_id($id);
+if($member == false) {
+  redirect_to(url_for('/users/members/index.php'));
 }
 
 if(is_post_request()) {
 
   // Save record using post parameters
   $args = $_POST['admin'];
-  $admin->merge_attributes($args);
-  $result = $admin->save();
+  $member->merge_attributes($args);
+  $result = $member->save();
 
   if($result === true) {
-    $_SESSION['message'] = 'The admin was updated successfully.';
-    redirect_to(url_for('/staff/admins/show.php?id=' . $id));
+    $_SESSION['message'] = 'The member was updated successfully.';
+    redirect_to(url_for('/users/members/show.php?id=' . $id));
   } else {
     // show errors
   }
@@ -33,24 +33,24 @@ if(is_post_request()) {
 
 ?>
 
-<?php $page_title = 'Edit Admin'; ?>
-<?php include(SHARED_PATH . '/staff_header.php'); ?>
+<?php $page_title = 'Edit Member'; ?>
+<?php include(SHARED_PATH . '/public_header.php'); ?>
 
 <div id="content">
 
-  <a class="back-link" href="<?php echo url_for('/staff/admins/index.php'); ?>">&laquo; Back to List</a>
+  <a class="back-link" href="<?php echo url_for('/users/members/index.php'); ?>">&laquo; Back to List</a>
 
-  <div class="admin edit">
-    <h1>Edit Admin</h1>
+  <div class="member edit">
+    <h1>Edit Member</h1>
 
-    <?php echo display_errors($admin->errors); ?>
+    <?php echo display_errors($member->errors); ?>
 
-    <form action="<?php echo url_for('/staff/admins/edit.php?id=' . h(u($id))); ?>" method="post">
+    <form action="<?php echo url_for('/users/members/edit.php?id=' . h(u($id))); ?>" method="post">
 
       <?php include('form_fields.php'); ?>
 
       <div id="operations">
-        <input type="submit" value="Edit Admin" />
+        <input type="submit" value="Edit Member" />
       </div>
     </form>
 
@@ -58,4 +58,4 @@ if(is_post_request()) {
 
 </div>
 
-<?php include(SHARED_PATH . '/staff_footer.php'); ?>
+<?php include(SHARED_PATH . '/public_footer.php'); ?>
